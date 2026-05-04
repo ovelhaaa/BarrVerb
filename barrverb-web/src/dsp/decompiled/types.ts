a8b7b7d02e31baad43ca9bf466399d28a58eb4af
Alexandre Lunelli da Silva <ovelhaaa@gmail.com>
2026-05-04 00:58:13 -0300

    feat: criar estrutura base do backend decompilado web

---
+++ barrverb-web/src/dsp/decompiled/types.ts
@@ -0,0 +1,14 @@
+export type DecompiledFamily = "MIDIVERB_II" | "MIDIFEX";
+
+export interface DecompiledFrameIO {
+  input: number;
+  outputL: number;
+  outputR: number;
+}
+
+export interface DecompiledState {
+  ram: Int16Array;
+  pointer: number;
+  lfo1: number;
+  lfo2: number;
+}
